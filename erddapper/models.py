7b2b19c4178a03b7bc3fff32fd88d7a39246ada9
Honza Rychly <honza@axiomdatascience.com>
2026-04-16 11:42:15 -0800

    Add validation alias in DatasetCreate model
    - allows for an older object key name

--- erddapper/models.py
+++ erddapper/models.py
@@ -1,12 +1,12 @@
 """Pydantic models for erddapper."""
 
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, Field, HttpUrl
 
 
 class DatasetCreate(BaseModel):
     """Payload for creating a new dataset."""
 
-    acdd_metadata: HttpUrl
+    acdd_metadata: HttpUrl = Field(..., validation_alias="acdd")
     sample_file: HttpUrl
 
     model_config = {
