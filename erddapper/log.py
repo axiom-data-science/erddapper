29a73b40765b04cfc86ed41976d0ad97e9b5223a
Honza Rychly <honza@axiomdatascience.com>
2026-04-16 11:11:32 -0800

    Fix logging config
    - LOGLEVEL env var can be used to overwrite the logging.conf
    - setup_logging is actually called when the service starts

--- erddapper/log.py
+++ erddapper/log.py
@@ -11,7 +11,8 @@ logger = logging.getLogger("erddapper")
 
 def setup_logging():
     """Initializes the project logging."""
-    logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO").upper())
     ref = importlib.resources.files("erddapper") / "logging.conf"
     with importlib.resources.as_file(ref) as path:
-        logging.config.fileConfig(path)
+        logging.config.fileConfig(path, disable_existing_loggers=False)
+    level = os.environ.get("LOGLEVEL", "INFO").upper()
+    logging.getLogger("erddapper").setLevel(level)
--- erddapper/main.py
+++ erddapper/main.py
@@ -1,15 +1,32 @@
 """FastAPI application for erddapper."""
 
+import logging
+
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from erddapper import __version__
+from erddapper.log import setup_logging
 from erddapper.routers import main_router
+
+
+logger = logging.getLogger("erddapper")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Service lifespan context manager."""
+    setup_logging()
+    logger.debug("Logging configured")
+    yield
 
 
 app = FastAPI(
     title="erddapper",
     description="A dapper dataset manager for ERDDAP.",
     version=__version__,
+    lifespan=lifespan,
 )
 
 app.include_router(main_router)
